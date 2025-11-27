b9b9437cba29709fd0cc99053d6b5c8de37966e4
luisMtz34 <luis.alvarez3969.alumnos.udg.mx>
2025-11-26 22:47:57 -0600

    endpoints updated

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    API_BASE_URL: "https://TU-BACK.fly.dev" 
+    API_BASE_URL: "https://clinicaappback.onrender.com"
 };
 // const CONFIG = { API_BASE_URL: "http://localhost:8082" };
