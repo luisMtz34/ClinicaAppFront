729a544cc9047ae3396402473040c31ff9d2736d
luisMtz34 <luis.alvarez3969.alumnos.udg.mx>
2025-11-29 18:36:23 -0600

    dashboard citas restructured

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    API_BASE_URL: "http://localhost:8082"
+    API_BASE_URL: "https://clinicaappback.onrender.com"
 };
 // const CONFIG = { API_BASE_URL: "http://localhost:8082" };
